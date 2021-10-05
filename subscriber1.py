fce069facd53329e428586ae75aad1b8cf1b999c
J-Reyes7 <88495139+J-Reyes7@users.noreply.github.com>
2021-10-05 17:54:30 -0400

    Add files via upload

---
+++ subscriber1.py
@@ -0,0 +1,27 @@
+import socket
+
+
+port = 5000
+format = 'utf-8'
+
+header = 64 
+host_ip = socket.gethostbyname(socket.gethostname())
+disconnect_msg = '!disconnnect'
+socket_sub1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+attach = (host_ip, port)
+
+socket_sub1.bind((host_ip,5001))
+socket_sub1.connect(attach)  
+
+def send(msg):
+    message = msg.encode(format)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(format)
+    send_length += b' ' * (header - len(send_length))
+    socket_sub1.send(send_length)
+    socket_sub1.send(message)
+
+while True:
+    send(input('Enter message:'))
+    print(socket_sub1.recv(3000).decode(format))
+
