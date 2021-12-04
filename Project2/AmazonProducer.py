2897dcbde95f95c5c3ef9252aa5dec5b6adff84e
J-Reyes7 <88495139+J-Reyes7@users.noreply.github.com>
2021-12-03 23:04:38 -0500

    Add files via upload

---
+++ Project2/AmazonProducer.py
@@ -0,0 +1,13 @@
+from kafka import KafkaProducer
+import yfinance as yf
+import pickle
+import time
+def advertise(event):
+    event.loc['ad'] = "If you own at least 15 shares of our stock you will receive Amazon Prime for FREE!"
+    return event
+raw_event = yf.download(tickers='AMZN', period='1d', interval='1m').iloc[-1]
+event = advertise(raw_event)
+producer = KafkaProducer(bootstrap_servers='localhost:9094',value_serializer=pickle.dumps)
+while True:
+    producer.send('Amazon', value=event)
+    time.sleep(5)
---
+++ Project2/AppleProducer.py
@@ -0,0 +1,13 @@
+from kafka import KafkaProducer
+import yfinance as yf
+import pickle
+import time
+def advertise(event):
+    event.loc['ad'] = "If you own at least 5 shares you will receive a 50% off the new Macbook Pro!"
+    return event
+raw_event = yf.download(tickers='AAPL', period='1d', interval='1m').iloc[-1]
+event = advertise(raw_event)
+producer = KafkaProducer(bootstrap_servers='localhost:9092',value_serializer=pickle.dumps)
+while True:
+    producer.send('Apple', value=event)
+    time.sleep(5)
